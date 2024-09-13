ced53ea36d7ddc2d84cf8ef7744810aba077dc7b
stavan54sk <stavan54sk@gmail.com>
2024-09-13 23:58:26 +0530

    updated locators

--- Chapter4/LocatorTest.spec.js
+++ Chapter4/LocatorTest.spec.js
@@ -12,14 +12,86 @@ test('CSS Locator Test', async ({ browser }) => {
    
 });
 
-test.only('XPath Locator Test', async ({ browser }) => {
+test('Get By Alt Text Test', async ({ browser }) => {
     const myContext = await browser.newContext();
 
     const myPage = await myContext.newPage();
 
-    await myPage.goto("https://www.facebook.com/login.php/");
+    await myPage.goto("https://bonigarcia.dev/");
     
-   await  myPage.locator('//[@id="email"]').click();
+   await  myPage.getByAltText('Boni García').click();
     
    
+});
+
+test('Get By Placeholder', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+    
+   await  myPage.getByPlaceholder('Email address or phone number').click();
+    
+   
+});
+
+test('Get By Role', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+    
+   await  myPage.getByRole('password').click();
+    
+   
+});
+
+test('Get By Text Contains', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+
+    await myPage.getByText('Sign Up').click();
+
+
+});
+
+test('Get By Text Exact', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+
+    await myPage.getByText('Sign Up', { exact: true }).click();
+
+
+});
+
+test('Get By Label', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+
+    await myPage.getByLabel('Password').click();
+
+
+});
+
+test.only('Get By Title', async ({ browser }) => {
+    const myContext = await browser.newContext();
+
+    const myPage = await myContext.newPage();
+
+    await myPage.goto("https://www.facebook.com/login/");
+
+    await myPage.getByTitle('Log in to Facebook').click();
+
+
 });
