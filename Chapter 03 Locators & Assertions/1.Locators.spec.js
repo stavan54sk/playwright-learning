4bb0dca5ab00e8ac4cc5bc090fbb86020a27a110
stavan54sk <stavan54sk@gmail.com>
2024-09-17 19:09:54 +0530

    updated locaters and completed lcoators

--- Chapter 03 Locators & Assertions/1.Locators.spec.js
+++ Chapter 03 Locators & Assertions/1.Locators.spec.js
@@ -1,27 +1,28 @@
 const { test, expect } = require('@playwright/test');
+const { link } = require('fs');
 
 test('getByRole', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
    await page.getByRole('button', { name: 'Submit' }).click();
-    
+
 });
 
 test('getByText', async ({ page }) => {
 
-    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
- 
-    await page.getByText('Default checkbox').click();
-     
- });
+   await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
- test('getByLabel', async ({ page }) => {
+   await page.getByText('Default checkbox').click();
+
+});
+
+test('getByLabel', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
    await page.getByLabel('Text input').fill('MyLabel')
-    
+
 });
 
 test('getByPlaceholder', async ({ page }) => {
@@ -29,7 +30,7 @@ test('getByPlaceholder', async ({ page }) => {
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
    await page.getByPlaceholder('Type to search...').fill('MyPlaceholder')
-    
+
 });
 
 test('getByAltText', async ({ page }) => {
@@ -38,17 +39,17 @@ test('getByAltText', async ({ page }) => {
 
    await page.waitForTimeout(5000)
 
-   await page.getByAltText('compass').hover();  
-   
-    
+   await page.getByAltText('compass').hover();
+
+
 });
 
 test('getByTitle', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
-   await page.getByTitle('Hands-On Selenium WebDriver with Java').isVisible(); 
-    
+   await page.getByTitle('Hands-On Selenium WebDriver with Java').isVisible();
+
 });
 
 
@@ -57,15 +58,15 @@ test('getByTitle', async ({ page }) => {
 //    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
 //    await page.getByTestId('Hands-On Selenium WebDriver with Java').isVisible(); 
-    
+
 // });
 
 test('css', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
-   await page.locator('[name="my-date"]').click() 
-    
+   await page.locator('[name="my-date"]').click()
+
 });
 
 test('xpath', async ({ page }) => {
@@ -73,7 +74,7 @@ test('xpath', async ({ page }) => {
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
    await page.locator('//*[text()="Web form"]').click();
-    
+
 });
 
 test('filter by has text', async ({ page }) => {
@@ -81,7 +82,7 @@ test('filter by has text', async ({ page }) => {
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
    await page.getByRole('link').filter({ hasText: 'Web form' }).click();
-    
+
 });
 
 test('filter by has not text', async ({ page }) => {
@@ -89,7 +90,7 @@ test('filter by has not text', async ({ page }) => {
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
    await expect(page.getByRole('link').filter({ hasNotText: 'Out of stock' })).toHaveCount(31);
-    
+
 });
 
 test('matching inside a locator', async ({ page }) => {
@@ -98,69 +99,68 @@ test('matching inside a locator', async ({ page }) => {
 
    const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
+   await div.getByRole('link').filter({ hasText: 'Web form' }).click();
+
 });
 
 test('matching 2 locator simultaneously', async ({ page }) => {
 
-   await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
+   await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await page.locator('[name="my-password"]').and(page.getByLabel('Password')).fill('password');
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
+
 });
 
 test('matching 2 locator alternatively', async ({ page }) => {
 
-   await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
+   await page.goto("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await page.getByLabel('My input').or(page.getByLabel('Text input')).fill('username')
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
+
 });
 
 test('matching only visible', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await page.getByRole('link', { name: 'Web form' }).locator('visible=true').click();
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
 });
 
 test('count items in a list', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await expect(page.getByRole('link')).toHaveCount(31);
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
+
+
 });
 
 test('get by nth item', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await page.getByRole('link').nth(3).click();
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
+//   await page.getByRole('link').last().click();
+
+//   await page.getByRole('link').first().click();
+
 });
 
 test('chaining filters', async ({ page }) => {
 
    await page.goto("https://bonigarcia.dev/selenium-webdriver-java/index.html");
 
-   const div = page.getByText('Chapter 3. WebDriver Fundamentals Web form Navigation Dropdown menu Mouse over');
+   await page.locator('//*[@class="row"]//div[contains(@class,"col")]')
+   .filter({ hasText: 'Web form' })
+   .filter({ hasNotText: 'Error form' })
+   .locator('//*[text()="Web form"]').click();
 
-   await div.getByRole('link').filter({hasText:'Web form'}).click();
-    
-});
+ });
 
 
 
