e724e51a0700de4a127b92d87bb6e2c2477316ba
stavan54sk <stavan54sk@gmail.com>
2024-10-01 10:28:10 +0530

    updated latest to chapter 9

---
+++ Chapter 09 API Testing/2.APIRequestContext.spec.js
@@ -0,0 +1,30 @@
+const { test, expect,request } = require('@playwright/test')
+
+test('first API test', async () => {
+
+    const newcontext =await request.newContext({
+        baseURL:'https://reqres.in'
+    });
+
+    const response = await newcontext.post('/api/users', {
+        data: {
+            "name": "test",
+            "job": "mytester"
+        }
+    })
+
+    expect(response.status()).toBe(201);
+
+    expect(response.ok()).toBeTruthy();
+
+    const responseBody =await response.json();
+
+    expect(responseBody.name).toBe('John Doe');
+
+    expect(responseBody.job).toBe('Software Developer');
+
+    expect(responseBody.id).toBeDefined();
+
+    expect(responseBody.createdAt).toBeDefined();
+
+})
